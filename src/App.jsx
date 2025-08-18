f8c10481a3acaf10c0635a010f33551e8c4a19a0
sami855-ux <samitale56@outlook.com>
2025-08-18 17:31:20 +0300

    changes are updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="font-geist">
+      <div className="bg-background p-6 rounded-lg">
+        This div uses the default background color
+      </div>
+
+      <div className="bg-card p-4 rounded-md">This div looks like a card</div>
+
+      <button className="bg-primary text-primary-foreground px-4 py-2 rounded font-mozilla">
+        Primary Button
+      </button>
+    </div>
+  )
 }
