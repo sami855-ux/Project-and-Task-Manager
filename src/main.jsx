2bf6125735793700fd65bc321162d31be692690f
sami855-ux <samitale56@outlook.com>
2025-09-29 19:26:37 +0300

    react-icons is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import { RouterProvider } from 'react-router'
-import CalendarPage from './components/Calander'
-import AppLayout from './components/AppLayout'
-import ProjectsPage from './components/Project/ProjectPage'
-import ProjectDetailsPage from './components/Project/ProjectDetail'
-
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import { RouterProvider } from "react-router";
+import CalendarPage from "./components/Calander";
+import AppLayout from "./components/AppLayout";
+import ProjectsPage from "./components/Project/ProjectPage";
+import ProjectDetailsPage from "./components/Project/ProjectDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-      path:"/",
-      element: <Dashboard/>
-      }, 
-    {
-    path: "/calendar",
-    element: <CalendarPage/>
-    },
-{
-  path:"/projects",
-  element: <ProjectsPage/>
-}, 
-{
-  path: "/project/deatil/:id",
-  element: <ProjectDetailsPage/>
-}
-    ]
-  }
-  
-])
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/calendar",
+        element: <CalendarPage />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/project/deatil/:id",
+        element: <ProjectDetailsPage />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}>
-        <App />
+      <App />
     </RouterProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
